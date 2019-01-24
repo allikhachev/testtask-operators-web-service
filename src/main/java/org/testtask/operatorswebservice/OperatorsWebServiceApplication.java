f8794a0a7c81378cef6a42ca912835912b923dc3
ocmwdt <ocmwdt@gmail.com>
2019-01-24 21:14:10 +0400

    initial commit

---
+++ src/main/java/org/testtask/operatorswebservice/OperatorsWebServiceApplication.java
@@ -0,0 +1,14 @@
+package org.testtask.operatorswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorsWebServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/testtask/operatorswebservice/OperatorsWebServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package org.testtask.operatorswebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OperatorsWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
